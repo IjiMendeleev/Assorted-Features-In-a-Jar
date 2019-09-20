a5306fdba126e89bb33b402557aa1f66767baa44
IjiMendeleev <32820323+IjiMendeleev@users.noreply.github.com>
2019-09-20 19:28:30 -0300

    Update BlockWC.java

--- src/main/java/com/evershy/afiajar/BlockWC.java
+++ src/main/java/com/evershy/afiajar/BlockWC.java
@@ -9,6 +9,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
@@ -18,17 +19,20 @@ import net.minecraft.tileentity.TileEntityMobSpawner;
 import net.minecraft.util.WeightedSpawnerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.event.ForgeEventFactory;
 import net.minecraftforge.fml.common.network.internal.EntitySpawnHandler;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockWC extends BlockBase 
 {
-    public BlockWC(String name, Material material, SoundType sound) 
+   
+	public BlockWC(String name, Material material, SoundType sound) 
 	{
     	super(name, material, sound);
         setCreativeTab(CreativeTabs.MISC);
-        setLightLevel(1F);                 
+        setLightLevel(1F);  
+        setTickRandomly(true);
 	}
 	
 	public boolean isOpaqueCube(IBlockState state)
@@ -41,10 +45,9 @@ public class BlockWC extends BlockBase
         return false;
     }
     
-    @Override
-	public void randomTick(net.minecraft.world.World worldIn, net.minecraft.util.math.BlockPos pos, net.minecraft.block.state.IBlockState state, java.util.Random random)
+    public void randomTick(net.minecraft.world.World worldIn, net.minecraft.util.math.BlockPos pos, net.minecraft.block.state.IBlockState state, java.util.Random random)
 	{
-       new EntityZombie(worldIn);
+      ForgeEventFactory.doSpecialSpawn(new EntityZombie(worldIn), worldIn,(float) pos.getX(),(float) pos.getY(),(float) pos.getZ());
 	}
 
 	public void registerModels() 
